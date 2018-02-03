7f937499361b134ab1f4706528be33ca36212a10
Marissa Santana <marissasantana@Sic-Em.local>
2018-02-03 11:30:16 -0500

    added 10 questions

--- magic_eight.py
+++ magic_eight.py
@@ -1,3 +1,22 @@
-def magic():
-    question = input("What is your question? To quit, enter 'quit'")
-    return question
+from random import randrange
+import random
+
+def Magic():
+    myboolean = True
+    while myboolean == True:
+        question = input("What is your question? To quit, enter 'quit'")
+
+        if question == 'quit':
+            break
+             # myboolean== False
+
+        elif "?" not in question:
+            print("I'm sorry, I can only answer questions.")
+            myboolean==False
+
+        else:
+            random_words = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes"]
+            magic_random = randrange(0, len(random_words))
+            print(random_words[magic_random])
+
+Magic()
